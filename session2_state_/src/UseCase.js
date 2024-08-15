3b038c0e0027e6694512cc33cdcaff942c850a80
Rajan <rajan.rana6@gmail.com>
2024-08-15 07:31:34 +0530

    update data

--- session2_state_/src/UseCase.js
+++ session2_state_/src/UseCase.js
@@ -1,16 +1,16 @@
 import { useState } from "react";
 
 function UseCase1() {
-    const [name,setName]=useState('Rajan');//initial value
-    const handleChange=()=>{
-        setName('Monika');
+    const [name, setName] = useState('Alex');//initial value
+    const handleChange = () => {
+        setName('Bob');
     }
-    return ( 
+    return (
         <div>
             <h3>{name}</h3>
             <button onClick={handleChange}>Change Name</button>
         </div>
-     );
+    );
 }
 
 export default UseCase1;
