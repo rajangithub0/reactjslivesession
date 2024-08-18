4e8fe6b370a9b84ed7a8e7c5d575a9e6a5b67ee4
Rajan <rajan.rana6@gmail.com>
2024-08-18 10:44:04 +0530

    from with regex and expression and validition

--- session9-fromandregex/src/main.jsx
+++ session9-fromandregex/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+  <App />
+  // </StrictMode>,
 )
