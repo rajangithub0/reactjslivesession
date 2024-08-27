3edbea3b916b644130374fff79cf5af39531c47d
Rajan <rajan.rana6@gmail.com>
2024-08-27 23:15:17 +0530

    basic of routes

---
+++ session12-nestedroute/src/App.jsx
@@ -0,0 +1,35 @@
+
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Header from './Components/Header'
+import Home from './Components/Home'
+import Login from './Components/Login'
+import Register from './Components/Register'
+import UsersList from './Components/UsersList'
+import UserDetails from './Components/UserDetails'
+
+function App() {
+
+
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='userslist' element={<UsersList />} />
+          <Route path='Details/:id' element={<UserDetails />} />
+
+        </Routes>
+
+      </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App
